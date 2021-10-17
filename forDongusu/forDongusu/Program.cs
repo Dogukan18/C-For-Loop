28f09f8f051a0bfdafd67c4e4bdb298795991672
Doğukan <60947113+Dogukan18@users.noreply.github.com>
2021-10-17 23:45:04 +0300

    Add files via upload

---
+++ forDongusu/forDongusu/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace forDongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*   int i;
+
+               for (i = 1; i <= 10; i++) 
+               {
+                   Console.WriteLine("Hello World");
+               }
+
+               Console.ReadLine(); 
+
+            /*  int i ;
+
+              for ( i = 1; i <=10;  i++)
+              {
+                  Console.WriteLine(i);
+              }
+              Console.ReadLine(); */
+
+            /* int i;
+             for ( i = 0; i <= 20; i+=2)
+             {
+                 Console.WriteLine(i);
+             }
+             Console.ReadLine(); */
+
+            //i++ = i'yi 1 artırır
+            //i+=2 = i'yi 2 artırır
+
+            //1'den 10'a kadar olan sayıların toplamı
+
+            /* int i;
+             int toplam = 0;
+
+             for ( i = 0; i <= 10 ; i++)
+             {
+                 toplam = toplam + i;
+             }
+
+             Console.WriteLine("Toplam: " + toplam);
+
+             Console.ReadLine(); */
+
+            //1 den 10 a kadar olan çift sayılar toplamı
+
+            /*  int i;
+              int toplam = 0;
+
+              for ( i = 0; i <=10 ; i+=2)
+              {
+                  toplam = toplam + i;
+              }
+              Console.WriteLine("Toplam: " + toplam);
+             Console.ReadLine();  */
+
+
+            //1 den 10 a kadar olan tek sayılar toplamı
+
+            /* int i;
+             int toplam = 0;
+             for ( i = 1; i <=10 ; i+=2)
+             {
+                 toplam = toplam + i;
+             }
+             Console.WriteLine("Toplam: "+toplam);
+             Console.ReadLine(); */
+
+
+            //9 a tam bölünen sayılar
+
+            /*   int i;
+               for ( i = 1; i <= 100; i++)
+               {
+                   if (i%9==0)
+                   {
+                       Console.WriteLine(i);
+                   }
+               }
+               Console.ReadLine(); */
+
+            //120 ye tam bölünen sayılar
+
+            /*  int sayi = 120;
+             for (int i = 1; i <= 120; i++)
+             {
+                 if (sayi%i==0)
+                 {
+                     Console.WriteLine(i);
+                 }
+             }
+             Console.ReadLine(); */
+
+          
+
+
+            
+
+    
+        
+        }
+    }
+}
